61202716d6ea6efa01789e8d8e34589d577e446f
James Pettit <james@academia.edu>
2020-12-07 00:13:46 -0800

    Build master

---
+++ docs/precache-manifest.f4eb0e6c413d65c721ea7256206d4845.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1299825a5fb3b16c9a0acbbb60b3c3",
+    "url": "/supermarket-tycoon/index.html"
+  },
+  {
+    "revision": "6552d7a95c76888b5705",
+    "url": "/supermarket-tycoon/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "d644417c8c15bfa0faf3",
+    "url": "/supermarket-tycoon/static/css/main.bd3ec610.chunk.css"
+  },
+  {
+    "revision": "6552d7a95c76888b5705",
+    "url": "/supermarket-tycoon/static/js/2.5a150cf1.chunk.js"
+  },
+  {
+    "revision": "85138567ef0c69ac615fbfc378c9912b",
+    "url": "/supermarket-tycoon/static/js/2.5a150cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d644417c8c15bfa0faf3",
+    "url": "/supermarket-tycoon/static/js/main.2cdd1848.chunk.js"
+  },
+  {
+    "revision": "84f7789ea2e8c2284895",
+    "url": "/supermarket-tycoon/static/js/runtime-main.f49b66e8.js"
+  }
+]);
